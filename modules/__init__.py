c5738ae2cca10ccf7da0e32f7a0efec9325cbbc3
kleberfreitas1990 <klebimfreitas@gmail.com>
2026-07-06 20:38:01 -0300

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,5 +1,1 @@
-# Torna a pasta 'modules' um pacote Python
-# Este arquivo está vazio para evitar imports circulares
-# Os módulos são importados diretamente nos arquivos que precisam deles
 
-__all__ = []
